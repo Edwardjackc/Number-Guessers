b250643f7f65f31c59aca1081f52b5bad35fd2f0
Alan Turing <anderson.gregory.mail@gmail.com>
2019-05-13 22:49:34 -0600

    Add error message functionality to number guess inputs
    co-authored-by: Edward Cheatham <edwardjackc@gmail.com>

--- main.js
+++ main.js
@@ -18,10 +18,19 @@ var inputGuessCh2 = document.querySelector('#player__guess--ch2');
 var currentGuessCh1 = document.querySelector('#latest-score__guess--ch1');
 var currentGuessCh2 = document.querySelector('#latest-score__guess--ch2');
 
+var rangeError = document.querySelector(".range__div")
+
 var randomNum 
 
 var guessHintCh1 = document.querySelector('#latest-score__output--left');
 var guessHintCh2 = document.querySelector('#latest-score__output--right');
+var latestScoreErrorCh1 = document.querySelector("#player__guess--ch1");
+var latestScoreErrorCh2 = document.querySelector("#player__guess--ch2");
+var playerError = document.querySelector(".player__div")
+var errorMessage = 
+    `<img src="error-icon.svg" height= 10px width=10px> <p>Please enter a valid range</p>`;
+var minRange;
+var maxRange;
 
 btnRangeUpdate.addEventListener('click', updateCorrectRange);
 btnClear.addEventListener('click', clearPlayerForm);
@@ -33,16 +42,24 @@ btnSubmit.addEventListener('click', function(){
     displayGuess ();
     checkResultsCh1 ();
     checkResultsCh2 ();
-  } //else
-  //error message
+  } 
 });
 
+// make room in a div and hide and toggle
 
 function updateRange() {
-  outputRangeMin.innerHTML = inputRangeMin.value || 1;
-  outputRangeMax.innerHTML = inputRangeMax.value || 100;
-  inputRangeMin.value = "";
-  inputRangeMax.value = "";
+  if(inputRangeMin.value >= inputRangeMax.value) {
+    var errorMessage = 
+    `<img src="error-icon.svg" height= 5px width=5px> <p>Please enter a valid range</p>`;
+    inputRangeMin.classList.add ("error-border");
+    inputRangeMax.classList.add ("error-border");
+    rangeError.innerHTML += errorMessage;
+  } else { 
+      outputRangeMin.innerHTML = inputRangeMin.value || 1;
+      outputRangeMax.innerHTML = inputRangeMax.value || 100;
+      inputRangeMin.value = "";
+      inputRangeMax.value = "";
+}
 } 
 
 function changeName() {
@@ -51,8 +68,8 @@ function changeName() {
 }
 
 function generateRandomNumber() {
-  var minRange = parseInt(outputRangeMin.innerHTML);
-  var maxRange = parseInt(outputRangeMax.innerHTML);
+  minRange = parseInt(outputRangeMin.innerHTML);
+  maxRange = parseInt(outputRangeMax.innerHTML);
   randomNum= Math.floor(Math.random() * (maxRange - minRange + 1)) + minRange;
   console.log(minRange);
   console.log(maxRange);
@@ -75,31 +92,41 @@ function resetGame() {
   changeName();
 }
 
-
-//query scorecard number and set to var for each ch
-//query each guess from each ch and set to var 
-//display each guess on a card
-
-
 function displayGuess (){
-currentGuessCh1.innerHTML = inputGuessCh1.value || 1;
-currentGuessCh2.innerHTML = inputGuessCh2.value || 100; 
+
+  if (checkLowerLimit(inputGuessCh1.value) && checkUpperLimit(inputGuessCh1.value)) {
+    currentGuessCh1.innerHTML = inputGuessCh1.value || 1;
+  } else {
+    latestScoreErrorCh1.classList.add ("error-border");
+    playerError.innerHTML += errorMessage;
+  } 
+  
+  if (checkLowerLimit(inputGuessCh2.value) && checkUpperLimit(inputGuessCh2.value)) {
+    currentGuessCh2.innerHTML = inputGuessCh2.value || 100;
+  } else{
+    latestScoreErrorCh2.classList.add ("error-border"); //add toggle to get rid of error message
+    playerError.innerHTML += errorMessage;
+  }
 }
 
-//target the random number
-//compare against guess
-//conditional on if high or low or correct
+function checkLowerLimit(guess){
+  return (guess > minRange);
+}
+
+function checkUpperLimit(guess){
+  return (guess < maxRange)
+}
 
 function checkResultsCh1 () {
   var playerOneGuess = parseInt(inputGuessCh1.value);
  if (playerOneGuess > randomNum){
       guessHintCh1.innerHTML = "That's too high!";
       } else if 
-    (playerOneGuess < randomNum){
+      (playerOneGuess < randomNum){
       guessHintCh1.innerHTML = "That's too low!";
       } else {
       guessHintCh1.innerHTML = "BOOM!"
-}
+      }
 }
 
 function checkResultsCh2 () {
@@ -117,7 +144,6 @@ function checkResultsCh2 () {
 function onlyAlphaNumeric (input){
   var lettersNumbers = /^[0-9a-zA-Z]+$/;
   if (input.value.match(lettersNumbers)) {
-    changeName ();
     return true;
   } else {
     //remove this alert upon entering error message
